0e123eb7c58360b83432c738e3f7bc8e9eb65439
Junior27VQ <favq1999@outlook.com>
2026-06-08 19:52:34 -0500

    first component

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,27 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 
+function Pelicula({titulo}){
+  return(
+    <div style={{border: '1px solid black',
+                  margin: '10px',
+                  padding: '10px',
+                  borderRadius: '5px'
+    }}>
+      <h2>{titulo}</h2>
+    </div>
+  )
+}
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
       <h1>Mis Peliculas Favoritas</h1>
+      <Pelicula titulo="El Padrino"/>
+      <Pelicula titulo="El Padrino II"/>
+      <Pelicula titulo="El Padrino III"/>
     </div>
   )
 }
